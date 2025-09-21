439ea7df379085da2c70b3b50fff019193c28981
Itay-Amzaleg <itay.amzaleg@campus.technion.ac.il>
2025-09-21 15:54:56 +0300

    added more explenations, organized the files, added statistics and pre proccessing part

---
+++ Data_Preprocessing.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def loadData(path):
+    return pd.read_csv(path)
+
+def addColumns(df):
+    df["is_big"] = df.apply(
+        lambda row: 1 if (row["sepal length (cm)"] > 3 and row["sepal width (cm)"] > 3) else 0, axis=1)
+    df["sepal avg"] = df[["sepal length (cm)", "sepal width (cm)"]].mean(axis=1)
+    df["petal avg"] = (df["petal length (cm)"] + df["petal width (cm)"]) / 2.0
+
+def statistics(df):
+    statistics = df.describe().round(2)
+    corr_matrix = df.corr()
+    print(statistics)
+    print(corr_matrix)
+    corr_pairs = (
+        corr_matrix.unstack()
+            .reset_index()
+            .rename(columns={"level_0": "var1", "level_1": "var2", 0: "correlation"})
+    )
+    corr_pairs = corr_pairs[corr_pairs["var1"] < corr_pairs["var2"]]
+    corr_pairs["correlation_abs"] = corr_pairs["correlation"].abs()
+    top5 = corr_pairs.nlargest(5,"correlation_abs").reset_index(drop=True).round(4)
+    least5 = corr_pairs.nsmallest(5,"correlation_abs").reset_index(drop=True).round(4)
+    print("Top 5 correlations are:")
+    print("\n".join(f'{row.Index+1}. {row.var1} and {row.var2}: {row.correlation_abs}' for i,row in enumerate(top5.itertuples())))
+    print("Least 5 correlations are:")
+    print("\n".join(f'{row.Index+1}. {row.var1} and {row.var2}: {row.correlation_abs}' for i,row in enumerate(least5.itertuples())))
+    #print_Corr_Bars(corr_pairs,top5, least5)
+
+
+def print_Corr_Bars(data, top5, least5):
+    xlabels = [f"{a} & {b}" for a,b in zip(data["var1"],data["var2"])]
+    x = [i*0.6 for i in range(len(xlabels))]
+    width = 0.2
+
+    fig, ax = plt.subplots(figsize=(12, 8))
+
+    corr_bar = plt.bar([i - (width / 2) for i in x], data["correlation"], width, label="Correlation")
+    plt.bar([i + (width / 2) for i in x], data["correlation_abs"], width, label="Correlation_abs")
+
+    ax.set_xticks(x)
+    ax.set_xticklabels(xlabels, rotation=45, ha="right", fontsize=10)
+    ax.set_ylabel("Value", fontsize=14)
+    ax.legend(loc="upper center")
+    ax.margins(x=0.01)
+    for row in top5.itertuples():
+        label = f"{row.var1} & {row.var2}"
+        idx = xlabels.index(label)
+        bar = corr_bar[idx]
+        ax.text(bar.get_x() + 0.2, bar.get_height() + 0.01, f'#{row.Index+1}', ha="center", va="bottom")
+    for spine in ax.spines.values():
+        spine.set_linewidth(1.5)
+    ax.title.set_text("Iris Correlation")
+    plt.tight_layout()
+    plt.show()
+
+
+
+def normalize(df):
+    #will be using minmax scaler
+    normalized = df
+    for column in df.columns:
+        normalized[column] = ((normalized[column] - normalized[column].min())/ (normalized[column].max() - normalized[column].min()))
+    return normalized
--- script.py
+++ script.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import sklearn
 import sklearn as sk
-from sklearn.cluster import KMeans
+import Data_Preprocessing as dataPre
+import itertools
+
+
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
@@ -12,32 +14,47 @@ pd.set_option('display.width', None)
 First project written in python
 
 Main goals:
+1.Being well organized 
+2.getting used to python syntax
+3.Working with SK_learn library
+4.Making simple visualizations
+5.Comparing several algorithms variations and get insightful conclusions
 
-1.getting used to work with python syntax:
-adding meaningfull and meaning less columns to the Iris data set
-calculating common statistical metrics
+Work Flow:
 
+1.Adding meaningfull and meaning less columns to the Iris data set calculating common statistical metrics
 
-2.running K-means with K=3 to on every 2 attributes combination and calculating the silhouette score, 
+2.running K-means with K=3 on every 2 attributes combination and calculating the silhouette score, purity, recall, precision and f1 score
+(since i know the true labels) and making conclusions about the "best" and "worst" combinations of attributes
+since i already implemented K-means in the past I will use the sk_learn model
 
+3.Visualise the results of Step 2 with graphs
+
+4.Running K-means with K=max_attributes to see if a higher dimensional clustering will lead to a better result
+since i never implemented KNN i will re-write the algorithm by myself
+
+5.Running KNN with different amount of neighbors (1 to max) and calculating recall, recision and f1 score to each model
+
+6.Visualise the results of Step 5 with graphs
+kMeans = sk.cluster.KMeans(n_clusters=3)
+        kMeans.fit(df_iris[pair])
 """
-
-
-def MessWithData(data):
-    df_iris["is_big"] = df_iris.apply(lambda row: 1 if (row["sepal length (cm)"] > 3 and row["sepal width (cm)"] > 3) else 0, axis=1)
-    df_iris["sepal avg"] = df_iris[["sepal length (cm)", "sepal width (cm)"]].mean(axis=1)
-    df_iris["petal avg"] = (df_iris["petal length (cm)"] + df_iris["petal width (cm)"]) / 2.0
-    return data
-
-def KmeansTwoFeatures(data, features):
-    kmeans = KMeans(n_clusters=len(features))
 
 
 if __name__ == '__main__':
     iris = sk.datasets.load_iris()
     df_iris = pd.DataFrame(iris.data, columns=iris.feature_names)
-    df_iris["target"] = iris['target']
-    df_iris = MessWithData(df_iris)
+    dataPre.addColumns(df_iris)
+    dataPre.statistics(df_iris)
+    normalized_iris = dataPre.normalize(df_iris)
+    #normalized_iris will be used solely for K-means (we don't want data leakage for KNN)
+
+
+    for pair in itertools.combinations(df_iris.columns, 2):
+        print(pair)
+
+    """
+    
 
     #splitting for training and testing data for KNN later
     df_iris_train = df_iris.sample(frac=0.8)
@@ -58,6 +75,8 @@ if __name__ == '__main__':
     plt.scatter(centroids[:,0], centroids[:,1], color='white', edgecolors='black', marker='o', s=500, alpha= 0.5)
 
     plt.show()
+    """
+
 
 
 
